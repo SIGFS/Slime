2e78b527bda0bd3303ea881d4318b7f09de2f498
Lillian Schutte <lillianschutte@gmail.com>
2023-02-01 11:48:07 -0600

    Added Boundaries to size script

--- Assets/Scripts/Player Scripts/SizeScript.cs
+++ Assets/Scripts/Player Scripts/SizeScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class SizeScript : MonoBehaviour
 {
-    [SerializeField] private float size = 1f;
+    [SerializeField] private float size = 3f;
     [SerializeField] private GameObject player;
     [SerializeField] private PlayerMovement move;
 
@@ -19,17 +19,17 @@ public class SizeScript : MonoBehaviour
     {
         //temp size increase / decrease using keypress
         //change to different method in future
-        if(Input.GetKeyDown(KeyCode.Equals)) {
-            size += 0.1f;
-            transform.position += new Vector3(0f, 0.05f, 0f);
-            transform.localScale = new Vector3(size, size, size);
-            move.maxSpeed -= 0.2f;
+        if(Input.GetKeyDown(KeyCode.Equals) && size < 2.99) {
+            size += 0.3f;
+            transform.position += new Vector3(0f, 0.15f, 0f);
+            transform.localScale = new Vector3(size, size, 0f);
+            move.maxSpeed -= 0.6f;
         }
-        if(Input.GetKeyDown(KeyCode.Minus)) {
-            size -= 0.1f;
-            transform.position -= new Vector3(0f, 0.05f, 0f);
+        if(Input.GetKeyDown(KeyCode.Minus) && size > 0.31) {
+            size -= 0.3f;
+            transform.position -= new Vector3(0f, 0.15f, 0f);
             transform.localScale = new Vector3(size, size, size);
-            move.maxSpeed += 0.2f;
+            move.maxSpeed += 0.6f;
         }
     }
 }
