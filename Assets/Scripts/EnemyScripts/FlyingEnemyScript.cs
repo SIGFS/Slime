1bee2c7654c064bcf604ea6ede8a43ac9e128916
jacobmill20 <jtm39290@ucmo.edu>
2023-02-02 15:23:51 -0600

    Finished Flying Enemy Script
    finished flying enemy script

--- Assets/Scripts/EnemyScripts/FlyingEnemyScript.cs
+++ Assets/Scripts/EnemyScripts/FlyingEnemyScript.cs
@@ -10,32 +10,49 @@ public class FlyingEnemyScript : MonoBehaviour
     public Transform leftGate;
     public Transform rightGate;
 
-    public SpriteRenderer enemySprite;
-    public BoxCollider2D enemyBox;
+    private SpriteRenderer enemySprite;
+    private BoxCollider2D enemyBox;
 
     private void Awake()
     {
-        
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+        enemySprite = enemy.GetComponent<SpriteRenderer>();
+        enemyBox = enemy.GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
     }
 
     private void Move()
     {
-        if(enemy.transform.position.x < leftGate.position.x)
+        if(enemy.transform.position.x <= leftGate.position.x)
         {
-            
+            if (!enemySprite.flipX)
+            {
+                enemyBox.offset = new Vector2(enemyBox.offset.x * -1, enemyBox.offset.y);
+                enemySprite.flipX = true;
+            }
             enemy.transform.Translate(Vector3.right * speed * Time.deltaTime);
+        } 
+        else if (enemySprite.flipX && enemy.transform.position.x > leftGate.position.x && enemy.transform.position.x < rightGate.position.x)
+        {
+            enemy.transform.Translate(Vector3.right * speed * Time.deltaTime);
+        } 
+        else if (!enemySprite.flipX && enemy.transform.position.x > leftGate.position.x && enemy.transform.position.x < rightGate.position.x)
+        {
+            enemy.transform.Translate(Vector3.left * speed * Time.deltaTime);
         }
+        else if (enemy.transform.position.x >= rightGate.position.x)
+        {
+            if (enemySprite.flipX)
+            {
+                enemyBox.offset = new Vector2(enemyBox.offset.x * -1, enemyBox.offset.y);
+                enemySprite.flipX = false;
+            }
+            enemy.transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+
     }
 }
