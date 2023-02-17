7fa778bc8ba90d378d510cf4d368d7285826c5a2
Lillian Schutte <lillianschutte@gmail.com>
2023-02-16 19:52:28 -0600

    Slime Well and Added SizeScript functionality
    Added Well prefab. Includes a script that checks if player is inside the well and slowly increases their size if they arent max size. Also add getSize(), setSize(), isMaxSize(), and isMinSize() methods to size script.

---
+++ Assets/Scripts/Level Scripts/WellScript.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WellScript : MonoBehaviour
+{
+    private float liquidRemaining;
+    private float delay;
+    private float delayMax = 1f;
+    BoxCollider2D well;
+
+    void Start() {
+        well = GetComponent<BoxCollider2D>();
+        liquidRemaining = 10f;
+        delay = 1f;
+    }
+
+    void Update() {
+        delay += Time.deltaTime;
+    }
+
+    void OnTriggerStay2D(Collider2D collider)
+    {
+        //checks if the collider colliding with it is attached to the player, if there is enough liquid remaining, if the cooldown is up, and if the player is
+        //not at the max size. If so, increases the player's size
+        if(collider.tag == "Player" && liquidRemaining > 0 && delay >= delayMax && !collider.gameObject.GetComponent<SizeScript>().isMaxSize()) {
+            delay = 0f;
+            collider.gameObject.GetComponent<SizeScript>().SizeChangeUp();
+            liquidRemaining--;
+            transform.localScale -= new Vector3(0f, 0.34f, 0f);
+        }
+    }
+}
--- Assets/Scripts/Player Scripts/SizeScript.cs
+++ Assets/Scripts/Player Scripts/SizeScript.cs
@@ -27,18 +27,18 @@ public class SizeScript : MonoBehaviour
     void Update()
     {
         //temp size increase / decrease using keypress
-        //change to different method in future
-        if(Input.GetKeyDown(KeyCode.Equals)) {
+        //commented out in in case of future need
+        /*if(Input.GetKeyDown(KeyCode.Equals)) {
             SizeChangeUp();
         }
         if(Input.GetKeyDown(KeyCode.Minus)) {
             SizeChangeDown();
-        }
+        }*/
     }
 
     public void SizeChangeUp()
     {
-        if(Data.size < 10)
+        if(!isMaxSize())
         {
             Data.size += 1;
             //Adjust position up to account for the change in size of object to prevent clipping through objects. Adjustment should be half of the size increase increment.
@@ -50,12 +50,30 @@ public class SizeScript : MonoBehaviour
 
     public void SizeChangeDown()
     {
-        if(Data.size > 1)
+        if(!isMinSize())
         {
             Data.size -= 1;
             //Adjust position down to put player back on floor. Adjustment should be half of the size decrease increment.
-            transform.position -= new Vector3(0f, -0.15f, 0f); 
+            transform.position -= new Vector3(0f, 0.15f, 0f); 
             transform.localScale = new Vector3((float)Data.size * 0.3f, (float)Data.size * 0.3f, 0f);
         }
+    }
+
+    public int getSize() {
+        return Data.size;
+    }
+
+    public void setSize(int size) {
+        Data.size = size;
+    }
+
+    public bool isMaxSize() {
+        if(Data.size == 10) return true;
+        return false;
+    }
+
+    public bool isMinSize() {
+        if(Data.size == 1) return true;
+        return false;
     }
 }
