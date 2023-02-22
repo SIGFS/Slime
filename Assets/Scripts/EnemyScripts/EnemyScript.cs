d8f8b42ca86d89a94414a0e769c9324d19b5c6f9
jacobmill20 <jtm39290@ucmo.edu>
2023-02-22 11:55:08 -0600

    Enemy Stuff
    Made enemies able to not be gated

--- Assets/Scripts/EnemyScripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScripts/EnemyScript.cs
@@ -6,12 +6,20 @@ public class EnemyScript : MonoBehaviour
 {
     public float speed;
 
+    public bool notGated;
+
     public GameObject enemy;
     public Transform leftGate;
     public Transform rightGate;
 
+    public Transform groundCheck;
+    public Transform wallCheck;
+    public LayerMask ground;
+
     private SpriteRenderer enemySprite;
     private Collider2D enemyBox;
+    public bool hitWall;
+    public bool hitEdge;
 
     private void Awake()
     {
@@ -22,41 +30,105 @@ public class EnemyScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (notGated)
+        {
+            checkGround();
+            checkWall();
+        }
+        
         if(enemy != null)
             Move();
     }
 
     private void Move()
     {
-        //Move back and forth between gates
-        if(enemy.transform.position.x <= leftGate.position.x)
+        if (!notGated)
         {
-            if (!enemySprite.flipX)
+            //Move back and forth between gates
+            if (enemy.transform.position.x <= leftGate.position.x)
+            {
+                if (!enemySprite.flipX)
+                {
+                    enemyBox.offset = new Vector2(enemyBox.offset.x * -1, enemyBox.offset.y);
+                    enemySprite.flipX = true;
+                }
+                MoveRight();
+            }
+            else if (enemySprite.flipX && enemy.transform.position.x > leftGate.position.x && enemy.transform.position.x < rightGate.position.x)
+            {
+                MoveRight();
+            }
+            else if (!enemySprite.flipX && enemy.transform.position.x > leftGate.position.x && enemy.transform.position.x < rightGate.position.x)
+            {
+                MoveLeft();
+            }
+            else if (enemy.transform.position.x >= rightGate.position.x)
+            {
+                if (enemySprite.flipX)
+                {
+                    enemyBox.offset = new Vector2(enemyBox.offset.x * -1, enemyBox.offset.y);
+                    enemySprite.flipX = false;
+                }
+                MoveLeft();
+            }
+        }
+        else
+        {
+            //Move back and forth between walls and edges
+            if ((hitEdge || hitWall) && !enemySprite.flipX)
             {
                 enemyBox.offset = new Vector2(enemyBox.offset.x * -1, enemyBox.offset.y);
                 enemySprite.flipX = true;
+
+                groundCheck.localPosition = new Vector3(-groundCheck.localPosition.x, groundCheck.localPosition.y, groundCheck.localPosition.z);
+                wallCheck.localPosition = new Vector3(-wallCheck.localPosition.x, wallCheck.localPosition.y, wallCheck.localPosition.z);
+                MoveLeft();
             }
-            enemy.transform.Translate(Vector3.right * speed * Time.deltaTime);
-        } 
-        else if (enemySprite.flipX && enemy.transform.position.x > leftGate.position.x && enemy.transform.position.x < rightGate.position.x)
-        {
-            enemy.transform.Translate(Vector3.right * speed * Time.deltaTime);
-        } 
-        else if (!enemySprite.flipX && enemy.transform.position.x > leftGate.position.x && enemy.transform.position.x < rightGate.position.x)
-        {
-            enemy.transform.Translate(Vector3.left * speed * Time.deltaTime);
-        }
-        else if (enemy.transform.position.x >= rightGate.position.x)
-        {
-            if (enemySprite.flipX)
+            else if (!enemySprite.flipX)
+            {
+                MoveLeft();
+            }
+            else if ((hitEdge || hitWall) && enemySprite.flipX)
             {
                 enemyBox.offset = new Vector2(enemyBox.offset.x * -1, enemyBox.offset.y);
                 enemySprite.flipX = false;
+
+                groundCheck.localPosition = new Vector3(-groundCheck.localPosition.x, groundCheck.localPosition.y, groundCheck.localPosition.z);
+                wallCheck.localPosition = new Vector3(-wallCheck.localPosition.x, wallCheck.localPosition.y, wallCheck.localPosition.z);
+                MoveRight();
             }
-            enemy.transform.Translate(Vector3.left * speed * Time.deltaTime);
+            else if (enemySprite.flipX)
+            {
+                MoveRight();
+            }
         }
 
     }
 
+    private void checkGround()
+    {
+        hitEdge = !Physics2D.Raycast(groundCheck.position, Vector2.down, 0.1f, ground);
+    }
+
+    private void checkWall()
+    {
+        Vector3 direction;
+        if (enemySprite.flipX)
+            direction = Vector3.right;
+        else
+            direction = Vector3.left;
+        
+        hitWall = Physics2D.Raycast(wallCheck.position, direction, 0.1f, ground);
+    }
+
+    private void MoveRight()
+    {
+        enemy.transform.Translate(Vector3.right * speed * Time.deltaTime);
+    }
+
+    private void MoveLeft()
+    {
+        enemy.transform.Translate(Vector3.left * speed * Time.deltaTime);
+    }
     
 }
