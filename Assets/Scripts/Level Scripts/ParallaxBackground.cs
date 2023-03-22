ee6f81b8e12a4afbb4cfe81b9816be2bf17990d8
Jake Hamilton <Hamilton456b@gmail.com>
2023-03-22 14:31:16 -0500

    parallax go brr

--- Assets/Scripts/Level Scripts/ParallaxBackground.cs
+++ Assets/Scripts/Level Scripts/ParallaxBackground.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class ParallaxBackground : MonoBehaviour
 {
+    private float length, startPos;
+    public Camera cam;
+    public float parallaxConstant;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float temp = (cam.transform.position.x * (1 - parallaxConstant));
+
+        float dist = (cam.transform.position.x * parallaxConstant);
+
+        transform.position = new Vector3(startPos + dist, transform.position.y, transform.position.z);
     }
+
 }
