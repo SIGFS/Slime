cfaed8e5508fb56b3738db3fb5abcf1fce05375e
jacobmill20 <jtm39290@ucmo.edu>
2023-04-06 13:31:06 -0500

    Moving Platform Slime Compatibility
    Slime moves with moving platforms

--- Assets/Scripts/Player Scripts/SlimeBallScript.cs
+++ Assets/Scripts/Player Scripts/SlimeBallScript.cs
@@ -29,27 +29,37 @@ public class SlimeBallScript : MonoBehaviour
             Vector2 collisionPoint = collision.GetContact(0).point;
             DirectionCheck(collisionPoint);
 
+            //Rotation default is 0, 0, 0
+            Vector3 rotation = new Vector3(0f, 0f, 0f);
+            
+            //Set rotation
             switch (hitDirection)
             {
                 case 0/*Right*/:
                     {
-                        Instantiate(bouncePrefab, new Vector2(collisionPoint.x, collisionPoint.y), Quaternion.Euler(0f, 0f, 90f));
+                        rotation = new Vector3(0f, 0f, 90f);
                         Debug.Log("Spawn Right");
                         break;
                     }
                 case 1/*Down*/:
                     {
-                        Instantiate(bouncePrefab, new Vector2(collisionPoint.x, collisionPoint.y), Quaternion.Euler(0f, 0f, 0f));
                         Debug.Log("Spawn Down");
                         break;
                     }
                 case 2/*Left*/:
                     {
-                        Instantiate(bouncePrefab, new Vector2(collisionPoint.x, collisionPoint.y), Quaternion.Euler(0f, 0f, -90f));
+                        rotation = new Vector3(0f, 0f, -90f);
                         Debug.Log("Spawn Left");
                         break;
                     }
             }
+            //Instantiate new bouncepad
+            GameObject newBouncePad = Instantiate(bouncePrefab, new Vector2(collisionPoint.x, collisionPoint.y), Quaternion.Euler(rotation));
+
+            //If collision is with a moving platform, set bouncepad as a child
+            if (collision.gameObject.tag == "Moving Platform")
+                newBouncePad.transform.parent = collision.transform;
+
             gameObject.SetActive(false);
         }
         if(collision.gameObject.tag == "Enemy")
